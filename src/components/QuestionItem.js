7fa99bff2428870097f3a185b48fd2826de53d04
Jerry Janvier <jerrythefilmmaker@gmail.com>
2024-02-29 17:58:41 -0500

    missing last deliverable

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,12 +1,10 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, handleDelete }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
-    <option key={index} value={index}>
-      {answer}
-    </option>
+    <option key={index} value={answer}> {answer} </option>
   ));
 
   return (
@@ -17,7 +15,7 @@ function QuestionItem({ question }) {
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={()=>{handleDelete(question)}}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,25 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({ setQuestions, questions }) {
+
+  function handleDelete (questionToDelete) {
+    let newQuestionList = questions.filter((item) => item !== questionToDelete)
+
+    fetch("http://localhost:4000/questions/" + questionToDelete.id, {
+      method: "DELETE",
+    })
+    .then((resp) => resp.json())
+    .then((data) => setQuestions(newQuestionList))
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+      {questions.map(question =>  
+        <QuestionItem handleDelete={handleDelete} key={question.id} question={question}/> )}
+      </ul>
     </section>
   );
 }
